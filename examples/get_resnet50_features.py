635750c1da9b50ffe94e8ba7e7fc966030a85d03
himjl <mil@mit.edu>
2026-01-06 23:22:56 -0500

    lint

--- examples/get_resnet50_features.py
+++ examples/get_resnet50_features.py
@@ -8,4 +8,6 @@ np.random.seed(0)
 image = PIL.Image.fromarray(np.random.randint(0, 255, (224, 224, 3), dtype=np.uint8))
 
 f = model.compute_features(images=[image])
-print(f) # array([0.20376027, 0.05251464, 0.00759117, ..., 0.33928823, 0., 0.70025563], shape=(2048,), dtype=float32)
+print(
+    f
+)  # array([0.20376027, 0.05251464, 0.00759117, ..., 0.33928823, 0., 0.70025563], shape=(2048,), dtype=float32)
