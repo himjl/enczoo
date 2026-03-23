8a95e1946ac9fd435d6180dbbaa66687d27aa543
Michael J. Lee <8962391+himjl@users.noreply.github.com>
2026-03-23 15:10:46 -0400

    Add CLIP ResNet50 (#9)
    * add convnextb
    
    * add pinned clip as dep
    
    * clip resnet50
    
    * lint
    
    * adjust agents

--- src/enczoo/__init__.py
+++ src/enczoo/__init__.py
@@ -1,6 +1,18 @@
-__all__ = ["ImageEncoding", "Pixels", "ResNet50", "AlexNet"]
+__all__ = [
+    "ImageEncoding",
+    "Pixels",
+    "ResNet50",
+    "AlexNet",
+    "ConvNeXtB",
+    "CLIPResNet50",
+]
 
 
 from enczoo.base import ImageEncoding
-from enczoo.neural_networks.torchvision import AlexNet, ResNet50
+from enczoo.neural_networks.torchvision import (
+    AlexNet,
+    CLIPResNet50,
+    ConvNeXtB,
+    ResNet50,
+)
 from enczoo.classic.pixels import Pixels
