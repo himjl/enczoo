ba822437b3dc5d6ca9f695fde848a609d64d0c35
Michael J. Lee <mil@mit.edu>
2026-03-29 14:42:12 -0400

    lazily import tf

--- examples/get_resnet50_features.py
+++ examples/get_resnet50_features.py
@@ -1,14 +1,18 @@
 import enczoo
 import PIL.Image
 import numpy as np
+from tqdm import trange
 
 
 np.random.seed(0)
 image = PIL.Image.fromarray(np.random.randint(0, 255, (224, 224, 3), dtype=np.uint8))
 
-model = enczoo.ResNet50(layer_name="avgpool")
+model = enczoo.AligNetViTB16()  # layer_name="avgpool")
 
-f = model.compute_features(images=[image])
+# model = model.to('cuda')
+for i in trange(1000):
+    f = model.compute_features(images=[image for _ in range(100)])
+
 print(
     f
 )  # array([0.20376027, 0.05251464, 0.00759117, ..., 0.33928823, 0., 0.70025563], shape=(2048,), dtype=float32)
--- src/enczoo/__init__.py
+++ src/enczoo/__init__.py
@@ -13,14 +13,23 @@ __all__ = [
 ]
 
 
+import os
+
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "3")
+os.environ.setdefault("TF_ENABLE_ONEDNN_OPTS", "0")
+os.environ.setdefault("TRANSFORMERS_NO_TF", "1")
+from enczoo.alignnet import AligNetViTB16, UnaligNetViTB16
+
+
 from enczoo.base import ImageEncoding
+from enczoo.classic.pixels import Pixels
 from enczoo.neural_networks.torchvision import (
     AlexNet,
     CLIPResNet50,
     ConvNeXtB,
     ResNet50,
 )
-from enczoo.alignnet import AligNetViTB16, UnaligNetViTB16
-from enczoo.classic.pixels import Pixels
 from enczoo.random_projection import RandomProjection
 from enczoo.transformers import CLIPViTB16, DINOv2ViTB14
+
+
